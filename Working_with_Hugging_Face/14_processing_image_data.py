e661222d8ca02a386d3f3c2653c3c488daf32f34
esragcetinkaya <esragcetinkaya@gmail.com>
2024-10-15 19:43:44 +0300

    14_10_24_commit

---
+++ Working_with_Hugging_Face/14_processing_image_data.py
@@ -0,0 +1,16 @@
+from transformers import image_transforms
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+
+original_image = Image.open("florence_city.jpg")
+# Create the numpy array
+numpy_image = np.array(original_image)
+
+print(numpy_image.shape)
+
+# Crop the center of the image
+cropped_image = image_transforms.center_crop(image=numpy_image, size=(200, 200))
+
+imgplot = plt.imshow(cropped_image)
+plt.show()
