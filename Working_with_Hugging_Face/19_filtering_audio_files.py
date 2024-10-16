205a4cb702b7e089c8e015b4973255f540685ba4
esragcetinkaya <esragcetinkaya@gmail.com>
2024-10-16 18:33:27 +0300

    15_10_24_commit_2

--- Working_with_Hugging_Face/19_filtering_audio_files.py
+++ Working_with_Hugging_Face/19_filtering_audio_files.py
@@ -1,26 +1,24 @@
 import librosa
-from datasets import load_dataset
-from torch.utils.data.sampler import BatchSampler, RandomSampler
-from torch.utils.data import DataLoader
 import numpy as np
+from datasets import load_dataset
 
-cv_13 = load_dataset("mozilla-foundation/common_voice_13_0", "hi", split="train")
-batch_sampler = BatchSampler(RandomSampler(cv_13), batch_size=32, drop_last=False)
-dataloader = DataLoader(cv_13, batch_sampler=batch_sampler)
+dataset = load_dataset("yagmurx/ataturk_voice_restorated",split='train')
 
-
+print("Dataset shape :",dataset.shape)
+print(dataset['audio'][1]['path'])
 # Create a list of durations
 old_durations_list = []
 
 # Loop over dataset
-for row in dataset['path']:
-    old_durations_list.append(librosa.get_duration(path=row))
+for row in range(len(dataset['audio'])):
+    old_durations_list.append(librosa.get_duration(y=dataset[row]['audio']['array'], sr=dataset[row]['audio']['sampling_rate']))
 
 # Creat a new column
 dataset = dataset.add_column("duration", old_durations_list)
 
+print(dataset['duration'])
 # Filter the dataset
-filtered_dataset = dataset.filter(lambda d: d < 6.0, input_columns=["duration"], keep_in_memory=True)
+filtered_dataset = dataset.filter(lambda d: d < 50, input_columns=["duration"], keep_in_memory=True)
 
 # Save new durations
 new_durations_list = filtered_dataset['duration']
